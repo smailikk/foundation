0601d27b12a1089c2f1033c42ca79391bbdbc613
smailikk <32063140+smailikk@users.noreply.github.com>
2017-12-18 16:54:18 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,193 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moto.ru</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+  </head>
+  <body>
+
+      <nav class="top-bar topbar-responsive">
+  <div class="top-bar-title">
+    <span data-responsive-toggle="topbar-responsive" data-hide-for="medium">
+      <button class="menu-icon" type="button" data-toggle></button>
+    </span>
+    <a class="topbar-responsive-logo" href="#"><strong>Moto.ru</strong></a>
+  </div>
+  <div id="topbar-responsive" class="topbar-responsive-links">
+    <div class="top-bar-right">
+      <ul class="menu simple vertical medium-horizontal">
+        <li><a href="#">Главная</a></li>
+        <li>
+          <button type="button" class="button hollow topbar-responsive-button"><a href="https://goo.gl/forms/aWLRy3HIHkeLR58O2">Подать объявление</a></button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+     <div class="fullscreen-image-slider">
+  <div class="orbit" role="region" aria-label="FullScreen Pictures" data-orbit>
+    <ul class="orbit-container">
+      <button class="orbit-previous">
+        <span class="show-for-sr">Previous Slide</span>
+        <span class="nav fa fa-chevron-left fa-3x"></span>
+      </button>
+      <button class="orbit-next">
+        <span class="show-for-sr">Next Slide</span>
+        <span class="nav fa fa-chevron-right fa-3x"></span>
+      </button>
+      <li class="is-active orbit-slide">
+        <img class="orbit-image" src="https://izobrazhenie.net/photo/0-0/2759_275141962.jpg" alt="Space">
+        <figcaption class="orbit-caption">
+          <h1>Продажа автомобилей <br>в Ульяновске </h1>
+        </figcaption>
+      </li>
+      <li class="orbit-slide">
+        <img class="orbit-image" src="http://www.hdcarwallpapers.com/download/2011_infiniti_m-1280x800.jpg" alt="Space">
+        <figcaption class="orbit-caption">
+          <h1>Продажа автомобилей <br>в Ульяновске </h1>
+        </figcaption>
+      </li>
+      <li class="orbit-slide">
+        <img class="orbit-image" src="https://im0-tub-ru.yandex.net/i?id=37ec9638b543f910fd53b6c39cc23480&n=13" alt="Space">
+        <figcaption class="orbit-caption">
+         <h1>Продажа автомобилей <br>в Ульяновске </h1>
+        </figcaption>
+      </li>
+    </ul>
+  </div>
+</div>
+<hr />
+        <div class="masonry-css">
+          <div class="masonry-css-item">
+            <img src="https://images.toyota-europe.com/ru/vehicle/landcruiser100/02d3361a-1e4d-46b5-a053-8423f25d129f/width/555/height/277/scale-mode/1/padding/0/background-colour/fff/image-quality/70/day-exterior-27_070.jpg" class="" height="" width="" alt="">
+          </div>
+              <div class="masonry-css-item">
+                <div class="callout">Land Cruiser 200 <br>
+                От 3 799 000 млн рублей</div>
+              </div>
+                <div class="masonry-css-item">
+                  <img src="http://altera-auto.ru/upload/iblock/49e/49e07fd4b66c5d7c1ae5845dd33edefc.png" class="" height="" width="" alt="">
+                </div>
+                    <div class="masonry-css-item">
+                    <div class="callout">Chevrolet orlando <br>
+                Механика от 642 000 рублей </div>
+                  </div>
+                      <div class="masonry-css-item">
+                        <img src="https://www.bmw.ru/content/dam/bmw/marketRU/bmw_ru/all-models/x-series/x1/2015/model-card/BMW-X1_ModelCard.png/_jcr_content/renditions/cq5dam.resized.img.585.low.time1456835645840.png" class="" height="" width="" alt="">
+                      </div>
+                          <div class="masonry-css-item">
+                        <div class="callout"> BMW 1 <br>
+                    1 500 000 млн рублей </div>
+                      </div>
+                        <div class="masonry-css-item">
+                          <img src="https://im0-tub-ru.yandex.net/i?id=5373582d236cdc3417ea190838fbff2e&n=13" class="" height="" width="" alt="">
+                        </div>
+                      <div class="masonry-css-item">
+                    <div class="callout"> INFINITI QX50  <br>
+                            2 215 000 рублей </div>
+                  </div>
+                <div class="masonry-css-item">
+                  <img src="http://www.mazda.ru/layout_static/color_block/model/m3/sedan/arctic.png" class="" height="" width="" alt="">
+                </div>
+                   <div class="masonry-css-item">
+                  <div class="callout">Mazda3 <br>
+                   1 642 000 рублей </div>
+                </div>
+              </div>
+              <div class="masonry-css">
+                <div class="masonry-css-item">
+                  <img src="http://topgir.com.ua/wp-content/uploads/2016/03/71521_Honda_Clarity_Fuel_Cell.jpg" class="" height="" width="" alt="">
+                </div>
+                      <div class="masonry-css-item">
+                        <div class="callout">Honda Clarity Fuel Cell <br>
+                        От 1 799 000 млн рублей</div>
+                      </div>
+                      <div class="masonry-css-item">
+                        <img src="https://www.volkswagen.ru/content/dam/vw-ngw/vw_pkw/importers/ru/models/tiguan-new/cutouts/Trendline_1920x726.png/jcr:content/renditions/original.transform/medplus/img.png" class="" height="" width="" alt="">
+                      </div>
+                  <div class="masonry-css-item">
+                  <div class="callout">Volkswagen Tiguan <br>
+               от 1 342 000 рублей </div>
+                </div>
+                <div class="masonry-css-item">
+                  <img src="https://im0-tub-ru.yandex.net/i?id=225c6a91bb22ca1c01e3c8e5ebc9b5a5&n=13" class="" height="" width="" alt="">
+                </div>
+            <div class="masonry-css-item">
+          <div class="callout"> Opel Insignia <br>
+            от 2 500 000 млн рублей </div>
+          </div>
+
+        <div class="masonry-css-item">
+          <img src="http://www.fordeumedia-a.ford.com/nas/gforcenaslive/rus/00b/yyb/images/resize618xrus00byybczbsbj(a)(a)hzn_1_0.png" class="" height="" width="" alt="">
+        </div>
+              <div class="masonry-css-item">
+            <div class="callout"> FORD EXPLORER  <br>
+         от 2 479 000 руб. </div>
+          </div>
+
+          <div class="masonry-css-item">
+            <img src="http://citroen-samavto.ru/upload/10503042373104231.png" class="" height="" width="" alt="">
+          </div>
+
+              <div class="masonry-css-item">
+            <div class="callout">КОМПЛЕКТАЦИИ НОВЫЙ C4 СЕДАН <br>
+             ОТ 999 000 РУБ. </div>
+          </div>
+      </div>
+
+      <hr />
+          <div id="engadget-footer-sitemap-container">
+        <footer id="engadget-footer-sitemap">
+          <div class="links">
+          </div>
+        </footer>
+      </div>
+      <div id="engadget-footer-social-container">
+          <footer id="engadget-footer-social">
+              <div class="footer-left">
+                  <div class="newsletter">
+         <img class="thumbnail" src="http://sferareklama.ru/uploads/smi/31feafa7c108370f2aa8i.jpg">
+              </div>
+             <p>© 2002—2017 Moto.ru (Ульяновская область)
+            Реклама на сайте.
+            Техподдержка.
+            Авто Ульяновск.
+            Авто.мото.ру: всё об автомобилях
+            Продажа автомобилей в Ульяновске – частные объявления, автосалоны Ульяновска, авторынки Ульяновска.</p>
+      </footer>
+</div>
+<div id="engadget-footer-contact-details-container">
+    <footer id="engadget-footer-contact-details">
+        <div class="footer-left">
+            <div class="contact-details">
+                    <h5>Контакты:</h5>
+                    <h6>тел: +7902 213 05 54</h6>
+                    <h6>тел: 8 842 555 55 55</h6>
+                    <h5>Адрес:</h5>
+                    <h6>Московское шоссе 129А</h6>
+                </ul>
+            </div>
+        </div>
+        <div class="footer-right">
+            <ul class="menu align-right">
+                <li><a href="#">В начало</a></li>
+            </ul>
+        </div>
+    </footer>
+</div>
+
+          </form>
+        </div>
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
